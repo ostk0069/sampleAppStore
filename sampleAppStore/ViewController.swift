53884fa5eb6ecc25bc1c6e8281b4fda7076b7fca
Takuma Osada <judgment9ttk069@gmail.com>
2019-05-04 16:21:52 +0900

    initial commit

---
+++ sampleAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleAppStore
+//
+//  Created by Takuma Osada on 2019/05/04.
+//  Copyright © 2019 Takuma Osada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
