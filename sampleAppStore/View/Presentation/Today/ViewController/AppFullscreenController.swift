042af7345637b358e5d4c69e136048eff9a4c4b8
Takuma Osada <judgment9ttk069@gmail.com>
2019-05-12 20:28:21 +0900

    fix for fixed closedButton

--- sampleAppStore/View/Presentation/Today/ViewController/AppFullscreenController.swift
+++ sampleAppStore/View/Presentation/Today/ViewController/AppFullscreenController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class AppFullscreenController: UITableViewController {
+class AppFullscreenController: UIViewController {
     
+    let tableView = UITableView(frame: .zero, style: .plain)
     var dismissHandler: (() -> ())?
     var todayItem: TodayItem?
     
+    let closeButton: UIButton = {
+        let button =  UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "close_button"), for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.clipsToBounds = true
+        view.addSubview(tableView)
+        tableView.fillSuperview()
+        tableView.dataSource = self
+        tableView.delegate = self
+        setupCloseButton()
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
@@ -23,21 +36,41 @@ class AppFullscreenController: UITableViewController {
         tableView.contentInset = .init(top: 0, left: 0, bottom: height, right: 0)
     }
     
-    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView.contentOffset.y < 0 {
             scrollView.isScrollEnabled = false
             scrollView.isScrollEnabled = true
         }
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    private func setupCloseButton() {
+        view.addSubview(closeButton)
+        closeButton.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: nil,
+            bottom: nil,
+            trailing: view.trailingAnchor,
+            padding: .init(
+                top: 12,
+                left: 0,
+                bottom: 0,
+                right: 0
+            ),
+            size: .init(
+                width: 80,
+                height: 40
+            )
+        )
+        closeButton.addTarget(self, action: #selector(handlerDismiss), for: .touchUpInside)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             let headerCell = AppFullscreenHeaderCell()
-            headerCell.closeButton.addTarget(self, action: #selector(handlerDismiss), for: .touchUpInside)
             headerCell.todayCell.todayItem = todayItem
             headerCell.todayCell.layer.cornerRadius = 0
             headerCell.clipsToBounds = true
@@ -49,15 +82,23 @@ class AppFullscreenController: UITableViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0 {
             return TodayController.cellSize
         }
-        return super.tableView(tableView, heightForRowAt: indexPath)
+        return UITableView.automaticDimension
     }
     
     @objc private func handlerDismiss(button: UIButton) {
         button.isHidden = true
         dismissHandler?()
     }
+}
+
+extension AppFullscreenController: UITableViewDataSource {
+    
+}
+
+extension AppFullscreenController: UITableViewDelegate {
+    
 }
--- sampleAppStore/View/Presentation/Today/ViewController/TodayController.swift
+++ sampleAppStore/View/Presentation/Today/ViewController/TodayController.swift
@@ -78,7 +78,7 @@ class TodayController: BaseListController {
                 guard let cell = self.appFullscreenController?.tableView.cellForRow(at: [0, 0]) as? AppFullscreenHeaderCell else {
                     return
                 }
-                cell.closeButton.alpha = 0
+                self.appFullscreenController.closeButton.alpha = 0
                 cell.todayCell.topConstraint?.constant = 24
                 cell.layoutIfNeeded()
         }, completion: { _ in
